9d00d359e1a3e9ee3ff4070920c8c9248ca83933
Ahmad Shah <155216264+ahmadicitian@users.noreply.github.com>
2024-11-09 09:33:57 -0800

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperFullStack.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
+        public string Image { get; set; }
+        public IFormFile file { get; set; }
+    }
+}
